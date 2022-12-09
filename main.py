3620e6c8caabf1a42cddeabe50e9eafbd9541da4
Umair <uak1911@gmail.com>
2022-12-10 00:12:33 +0530

    added movement to the player

--- main.py
+++ main.py
@@ -19,5 +19,25 @@ while gameon:
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             gameon = False
+
+        if (event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_a or event.key == pygame.K_LEFT):
+                if(playerx >20):
+                   playerx -= 20
+                
+
+            if(event.key == pygame.K_d or event.key == pygame.K_RIGHT):
+                if (playerx < 930):
+                    playerx += 20
+
+            if(event.key == pygame.K_w or event.key == pygame.K_UP):
+                if(playery > 20):
+                    playery -= 20
+                    
+
+            if(event.key == pygame.K_s or event.key == pygame.K_DOWN):
+                    if (playery < 775):
+                        playery += 20
+
     player()
     pygame.display.update()
